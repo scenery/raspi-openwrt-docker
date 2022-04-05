36ac791c6200aa9cce1378410b0ae01fbd4949f9
ATP <32787144+scenery@users.noreply.github.com>
2022-04-06 00:14:02 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -30,9 +30,10 @@ rm -rf ../lean/luci-theme-argon
 popd
 
 # Mod zzz-default-settings
-pushd package/emortal/default-settings/files
+pushd package/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
-sed -i '/openwrt_luci/d' zzz-default-settings
+export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
 popd
 
 # Change default shell to zsh
