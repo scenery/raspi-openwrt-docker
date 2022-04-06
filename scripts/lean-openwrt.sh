1639ae432ad26a0533f73f36b58e6b6df80b697f
ATP <32787144+scenery@users.noreply.github.com>
2022-04-06 16:51:47 +0800

    Update lean-openwrt.sh

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -17,12 +17,6 @@ popd
 mkdir package/community
 pushd package/community
 
-# Add Lienol's Packages
-# git clone --depth=1 https://github.com/Lienol/openwrt-package
-
-# Add luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
-
 # Add luci-theme-argon
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
 git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
