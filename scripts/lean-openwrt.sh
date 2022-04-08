c40777ff3aaa10ff7d6ccb9137245d7c17618ae8
ATP <32787144+scenery@users.noreply.github.com>
2022-04-09 03:14:11 +0800

    add luci-app

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -11,9 +11,11 @@
 pushd package/lean
 # Add luci-app-ssr-plus
 git clone --depth=1 https://github.com/fw876/helloworld
-
 # Add luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+# Add luci-app-smartdns
+git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns
+
 popd
 
 # Clone community packages to package/community
